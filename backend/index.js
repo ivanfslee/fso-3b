6cfbeaa58ef8049bdb1b9e7482845a22db6a952b
ivanfslee <ivanfslee@gmail.com>
2023-07-22 16:11:24 -0700

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,11 @@ let notes = [
     id: 3,
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true
+  },
+  {
+    id: 4,
+    content: "test note",
+    important: false
   }
 ]
 
